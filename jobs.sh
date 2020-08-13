1eb4ce37e0f17e9bdcb6fed3cbdc7262f41ce838
Aashis Khanal <sraashis@gmail.com>
2020-08-13 11:51:55 -0400

    Long to float in MSE loss.

--- jobs.sh
+++ jobs.sh
@@ -2,4 +2,3 @@
 python main.py -p train -b 32 -r 16 -wm binary -nw 16 -e 51 -nch 2
 python main.py -p train -b 32 -r 16 -wm multi -nw 16 -e 51 -nch 2
 python main.py -p train -b 32 -r 16 -wm multi_reg -nw 16 -e 51 -nch 2
-cd

a6e2dbcdac0da721b6d2c70840d6b2d7b3368639
Aashis Khanal <sraashis@gmail.com>
2021-01-07 00:42:24 -0500

    Shortened.

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 import argparse
-from easytorch.etargs import ap
-ap = argparse.ArgumentParser(parents=[ap], add_help=False)
-
-import dataspecs as dspec
-
-from easytorch import EasyTorch
+from easytorch import EasyTorch, default_args
 from classification import KernelDataset, KernelTrainer
+import os
 
-ap = argparse.ArgumentParser(parents=[ap], add_help=False)
+sep = os.sep
+
+ap = argparse.ArgumentParser(parents=[default_args], add_help=False)
+ap.add_argument("-nch", "--num_channel", default=3, type=int, help="Number of channels of input image.")
 ap.add_argument('-wm', '--which_model', default='multi', type=str, help='Which model to load.')
+ap.add_argument('-sz', '--model_scale', default=1, type=int, help='Mode width scale')
 
-dataspecs = [dspec.V7CF_DATASET]
-runner = EasyTorch(dataspecs, ap)
+# --------------------------------------------------------------------------------------------
+V7CF_DATASET = {
+    'name': 'v7_lab_cf',
+    'data_dir': 'v7_lab_cf' + sep + 'images',
+    'mask_dir': 'v7_lab_cf' + sep + 'masks',
+    'label_dir': 'v7_lab_cf' + sep + 'labels',
+    'split_dir': 'v7_lab_cf' + sep + 'splits',
+    'label_getter': lambda file_name: file_name.split('.')[0] + '.png',
+    'mask_getter': lambda file_name: file_name.split('.')[0] + '.png',
+}
+runner = EasyTorch([V7CF_DATASET], ap, dataset_dir='datasets')
 
 if __name__ == "__main__":
     runner.run(KernelDataset, KernelTrainer)

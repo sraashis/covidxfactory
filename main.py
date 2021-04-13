59c2fc1b70845ff2abf9b9812111cf3f1e09f766
Aashis Khanal <sraashis@gmail.com>
2021-04-12 21:23:47 -0400

    Func data getter.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import argparse
-from easytorch import EasyTorch, default_ap
-from classification import KernelDataset, KernelTrainer
 import os
+
+from easytorch import EasyTorch, default_ap
+
+from classification import KernelDataset, KernelTrainer
 
 sep = os.sep
 
@@ -10,7 +12,14 @@ ap.add_argument("-nch", "--num_channel", default=3, type=int, help="Number of ch
 ap.add_argument('-wm', '--which_model', default='multi', type=str, help='Which model to load.')
 ap.add_argument('-sz', '--model_scale', default=1, type=int, help='Mode width scale')
 
+
 # --------------------------------------------------------------------------------------------
+def get_label(file_name):
+    return file_name.split('.')[0] + '.png'
+
+
+def get_mask(file_name):
+    return file_name.split('.')[0] + '.png'
 
 
 V7CF_DATASET = {
@@ -19,8 +28,8 @@ V7CF_DATASET = {
     'mask_dir': 'v7_lab_cf' + sep + 'masks',
     'label_dir': 'v7_lab_cf' + sep + 'labels',
     'split_dir': 'v7_lab_cf' + sep + 'splits',
-    'label_getter': lambda file_name: file_name.split('.')[0] + '.png',
-    'mask_getter': lambda file_name: file_name.split('.')[0] + '.png',
+    'label_getter': get_label,
+    'mask_getter': get_mask,
 }
 runner = EasyTorch([V7CF_DATASET], ap, dataset_dir='datasets', split_ratio=[0.6, 0.2, 0.2])
 
